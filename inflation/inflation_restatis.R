fc4b281d2e4e453a2ea5f1859567d1016efd8f3e
Michael N <wahlatlas@users.noreply.github.com>
2024-01-13 19:19:07 +0100

    ggplot styling

--- inflation/inflation_restatis.R
+++ inflation/inflation_restatis.R
@@ -41,6 +41,7 @@ gen_val2var("CC13Z1",
 # Tabellen, die den COICOP-10-Steller enthalten
 gen_find(term = "CC13Z1", category = "tables")
 
+
 # Code: 61111-0006
 # Verbraucherpreisindex: Deutschland, Monate, 
 # Klassifikation der Verwendungszwecke des Individualkonsums 
@@ -58,7 +59,7 @@ iKonsum = gen_table("61111-0006",
           compress = TRUE,
           language = "en")
 
-title = iKonsum$'3_variable_code...17'[1]
+title = iKonsum$`3_variable_code...17`[1]
 
 select(iKonsum, 5,12,13,17,18)
 
@@ -69,7 +70,7 @@ select(iKonsum, 5,12,13,17,18)
 # aus 2020 MONAT01 wird 2020-01-01
 iKonsum <- transform(iKonsum, 
                      dateObj=as.Date(paste(time,
-                                           substr(`2_variable_code...12`,6,7),
+                                           substr(`2_variable_code...12`, 6,7),
                                            "01",
                                            collapse = NULL, sep="-")))
 
@@ -79,12 +80,14 @@ ggplot(data=iKonsum, aes(x=dateObj,
                          y=PREIS1__Consumer_price_index__2020.100, 
                          group=1)) +
   geom_line() +
+  geom_line(linewidth=1.5) +
   ggtitle(title) +
   scale_x_date(date_breaks = "3 months", 
                date_minor_breaks = "1 month",
                labels = scales::label_date_short()) +
   xlab("") + 
-  ylab("Consumer Price Index (2020=100)")
+  ylab("Consumer Price Index (2020=100)") +
+  theme(text = element_text(size=15)) 
 
 
 # Aktualisierungen
@@ -93,3 +96,38 @@ gen_modified_data(type = "tables", date = "month_before")
 
 # Beschreibung zur Tabelle
 gen_metadata(code = "61111-0006", category = "Table")
+
+
+
+# LÄNGERE ZEITREIHEN
+# Visualisierung Herren-, Damen-, Kinderbekleidung
+
+# COICOP-5-Steller (2020=100) ab 2015 verfügbar
+PreiseBekleidung = gen_table("61111-0006", 
+                    startyear = 2015, 
+                    classifyingvariable1 = "CC13A5",
+                    classifyingkey1 = "CC13-0312*",
+                    compress = TRUE,
+                    language = "en")
+
+PreiseBekleidung <- transform(PreiseBekleidung, 
+                     dateObj=as.Date(paste(time,
+                                           substr(`2_variable_code...12`,6,7),
+                                           "01",
+                                           collapse = NULL, sep="-")))
+
+ggplot(data=PreiseBekleidung, aes(x=dateObj, 
+                         y=PREIS1__Consumer_price_index__2020.100, 
+                         colour=`X3_variable_code...17`)) +
+  geom_line() +
+  scale_color_manual(values=c("chocolate1", "blue3", "cyan4")) +
+  geom_line(linewidth=1) +
+  ggtitle("CC13-0312*") +
+  scale_x_date(date_breaks = "1 year", 
+               date_minor_breaks = "3 months",
+               date_labels = "%Y") +
+  xlab("") + 
+  ylab("Consumer Price Index (2020=100)") +
+  theme(legend.position=c(0.1, 0.86)) +
+  theme(legend.title=element_blank()) +
+  theme(text = element_text(size=15)) 
